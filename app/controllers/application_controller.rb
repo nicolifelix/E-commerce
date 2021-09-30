c461f15dffa47d2285b7e90d89612f69b7da9f67
nicolifelix <nicolifelix@hotmail.com>
2021-09-30 13:57:15 -0300

    ￼ Config Password Reset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    before_action :configure_permited_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permited_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    end    
+
 end
