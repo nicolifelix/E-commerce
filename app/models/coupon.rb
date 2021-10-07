ea24731a890e60e7def672adae65a73abc8e2a2f
nicolifelix <nicolifelix@hotmail.com>
2021-10-07 15:19:53 -0300

    :incoming_envelope:Model Coupon and config model User

---
+++ app/models/coupon.rb
@@ -0,0 +1,10 @@
+#rails g Coupon code status:integer "discount_value:decimal{5,2}" due_date:datetime
+class Coupon < ApplicationRecord
+
+    validates :code, presence: true, uniqueness: { case_sensitive: false }
+    validates :status, presence: true
+    validates :due_date, presence: true
+    validates :discount_value, presence: true, numericality: { greater_than: 0}
+
+    enum status: { active:1, inactive:2 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_151306) do
+ActiveRecord::Schema.define(version: 2021_10_07_175708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "code"
+    t.integer "status"
+    t.decimal "discount_value", precision: 5, scale: 2
+    t.datetime "due_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -29,6 +38,15 @@ ActiveRecord::Schema.define(version: 2021_10_07_151306) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["system_requirement_id"], name: "index_games_on_system_requirement_id"
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -78,4 +96,6 @@ ActiveRecord::Schema.define(version: 2021_10_07_151306) do
   end
 
   add_foreign_key "games", "system_requirements"
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
 end
